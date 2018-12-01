04618f40b49ad8efa16eef21d8314a6461c69fae
Unknown <1103366093@qq.com>
2018-12-01 14:52:17 +0800

    renew stop word and .51

--- train.py
+++ train.py
@@ -20,31 +20,33 @@ import time
 
 from capsule import *
 
-USE_CUDA = True
-embedding_dim = 300
-LR = 0.001
-num_classes = 30
-remove_stop_words = True
-
 
 def read_file():
     data = pd.read_csv('data/train.csv')
     #data['content'] = data.content.map(lambda x: ''.join(x.strip().split()))
+    data['content'] = data.content.map(lambda x: ''.join(x.strip().split()))
     if remove_stop_words:
-        with open('data/hit_stop_words.txt') as f:
+        with open('data/stop_words.txt') as f:
             stop_words = set([l.strip() for l in f])
             #print(stop_words)
         data['content'] = data.content.map(
-            lambda x: ''.join([e for e in x.strip().split()[0] if e not in stop_words]))
-    else:
-        data['content'] = data.content.map(lambda x: ''.join(x.strip().split()))
+            lambda x: ''.join([e for e in x if e not in stop_words]))
 
     # 把主题和情感拼接起来，一共10*3类
+    #print(data.content[0:20])
+    subj_to_id = {'动力':0, '价格':1, '内饰':2, '配置':3, '安全性':4, '外观':5, '操控':6, '油耗':7, '空间':8, '舒适性':9}
+    sent_to_id = {-1:0, 0:10, 1:20}
+    data['subject'] = data['subject'].apply(lambda x: subj_to_id.get(x))
+    data['sentiment_value'] = data['sentiment_value'].apply(lambda x: sent_to_id.get(x))
+    data['label'] = data['subject'] + data['sentiment_value']
+    '''
     data['label'] = data['subject'] + data['sentiment_value'].astype(str)
     subj_lst = list(filter(lambda x: x is not np.nan, list(set(data.label))))
-    subj_lst_dic = {value: key for key, value in enumerate(subj_lst)}
-    data['label'] = data['label'].apply(lambda x: subj_lst_dic.get(x))
-
+    subj_dic = {value: key for key, value in enumerate(subj_lst)}
+    data['label'] = data['label'].apply(lambda x: subj_dic.get(x))
+    #print(data[90:100])
+    '''
+    #print(data[90:100])
     return data
 
 
@@ -53,6 +55,7 @@ def process_data(data):
     data_tmp = data.groupby('content').agg({'label': lambda x: set(x)}).reset_index()
     # [[1,0,0],[0,1,0],[0,0,1]]
     # 可能有多标签则[[1,1,0],[0,1,0],[0,0,1]]
+    #print(data_tmp[0:100])
     mlb = MultiLabelBinarizer()
     data_tmp['trans_label'] = mlb.fit_transform(data_tmp.label).tolist()
     Y = np.array(data_tmp.trans_label.tolist())
@@ -61,12 +64,13 @@ def process_data(data):
 
     bow = BOW(data_tmp.content.apply(jieba.lcut).tolist(), min_count=1, maxlen=100)  # 长度补齐或截断固定长度100
 
-    #word2vec = Word2Vec(data_tmp.content.apply(jieba.lcut).tolist(), size=300, min_count=1)
-    #word2vec.wv.save_word2vec_format('data/w2v.txt', binary=False)
-    word2vec = gensim.models.KeyedVectors.load_word2vec_format('data/w2v.txt') # 读取txt文件的预训练词向量
+    word2vec = Word2Vec(data_tmp.content.apply(jieba.lcut).tolist(), size=300, min_count=1)
+    word2vec.wv.save_word2vec_format('data/w2v.txt', binary=False)
+    #word2vec = gensim.models.KeyedVectors.load_word2vec_format('data/w2v.txt') # 读取txt文件的预训练词向量
 
     vocab_size = len(bow.word2idx)
     embedding_matrix = np.zeros((vocab_size + 1, embedding_dim))
+    #print(bow.word_count)
     for key, value in bow.word2idx.items():
         if key in word2vec.wv.vocab:  # Word2Vec训练得到的的实例需要word2vec.wv.vocab
             embedding_matrix[value] = word2vec.wv.get_vector(key)
@@ -108,12 +112,12 @@ class BOW(object):
 
     def __word_count(self):
         wc = {}
-        for ws in tqdm(self.X, desc='   Word Count'):
-            for w in ws:
-                if w in wc:
-                    wc[w] += 1
+        for text in tqdm(self.X, desc='   Word Count'):
+            for x in text:
+                if x in wc:
+                    wc[x] += 1
                 else:
-                    wc[w] = 1
+                    wc[x] = 1
         self.word_count = {i: j for i, j in wc.items() if j >= self.min_count}
 
     def __idx(self):
@@ -139,9 +143,10 @@ def train_capnet(EPOCH, train_loader, test_content_tensor, Y_test):
 
     it = 1
     flag = 1
-    threshold = 0.3
+    threshold = 0.2
     max_test_f1 = 0
     f1 = [0] * 9
+    capnet.train()
     for epoch in tqdm_notebook(range(EPOCH)):
         for batch_id, (data, target) in enumerate(train_loader):
             # print(len(target.numpy()))
@@ -151,6 +156,7 @@ def train_capnet(EPOCH, train_loader, test_content_tensor, Y_test):
             loss = loss_func(output, target)
             # print(np.rint(output.cpu().data.numpy()))
             if it % 50 == 0:
+                capnet.eval()
                 Y_test_pred = capnet(test_content_tensor.cuda()).cpu().data.numpy()
                 for threshold_ in np.arange(0.1, 1, 0.1):
                     YY = copy.deepcopy(Y_test_pred)
@@ -181,12 +187,20 @@ def train_capnet(EPOCH, train_loader, test_content_tensor, Y_test):
     print(f1)
 
 
+USE_CUDA = True
+embedding_dim = 300
+LR = 0.001
+num_classes = 30
+remove_stop_words = True
+
+
 if __name__ == '__main__':
     warnings.filterwarnings("ignore")
     np.set_printoptions(precision=2, suppress=True, threshold=np.nan)
 
     data = read_file()
     X, Y, embedding_matrix, vocab_size = process_data(data)
+    #print(embedding_matrix)
 
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=3)
 
@@ -207,12 +221,9 @@ if __name__ == '__main__':
     # 开始跑模型
     EPOCH = 30
     train_capnet(EPOCH, train_loader, test_content_tensor, Y_test)
-    capnet = torch.load('model_saved/capnet.pkl')
-    #capnet = Capsule_Main(embedding_matrix, vocab_size).cuda()
-    #capnet.load_state_dict(torch.load('model_saved/capnet.pkl'))
-    #print(capnet.state_dict())
+    capnet = torch.load('model_saved/capnet.pkl').eval()
     Y_test_pred = capnet(test_content_tensor.cuda()).cpu().data.numpy()
-    threshold = 0.3
+    threshold = 0.2
     Y_test_pred[Y_test_pred >= threshold] = 1
     Y_test_pred[Y_test_pred < threshold] = 0
     test_f1 = f1_score(Y_test, Y_test_pred, average='micro')
